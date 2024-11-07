9bf1a52098a2444cf206865f5607345188423c57
Ahmed Mohamed <85865532+Abuelyazeed@users.noreply.github.com>
2024-11-07 16:10:44 +0200

    nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from './pages';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Home</h1>,
-  },
-  {
-    path: '/about',
-    element: (
-      <div>
-        <h1>About Page</h1>
-      </div>
-    ),
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ],
   },
 ]);
 function App() {
